53b2674495d0dcc976d228bfc768c962cce1583d
Heidi Seo <heidiseo1@gmail.com>
2019-05-16 17:45:13 +0100

    created actions for users, dogs and bookings controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,39 @@
 class BookingsController < ApplicationController
+
+  before_action :set_booking, only: [:show, :destroy]
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:user_id])
+    @booking.user = @user
+    @booking.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :comment, :user_id, :dog_id)
+  end
+end
+
 end
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,50 @@
 class DogsController < ApplicationController
+
+  before_action :set_dog, only: [:show, :edit, :update, :destroy]
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @dog.update(dog_params)
+      redirect_to dog_path(@dog)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dog.destroy
+    redirect_to dogs_path
+  end
+
+  private
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:photo, :name, :breed, :description, :rate, :age)
+  end
 end
