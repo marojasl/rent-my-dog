132a4109fab5760fdb0f3260582c5746176598bf
Maria Antonia Rojas <marojaslondono@gmail.com>
2019-05-20 12:47:11 +0100

    geocoding map with postcode search

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,9 @@
 class Dog < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   mount_uploader :photo, PhotoUploader
-  validates :name, :breed, :photo, presence: true
+  validates :name, :breed, :photo, :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
