50d43217bf379052c0350e3417cf760d6aed40a0
Maria Antonia Rojas <marojaslondono@gmail.com>
2019-05-21 12:38:59 +0100

    Calendar selector for dates

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,15 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/themes/airbnb.css" // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin.js"
+
+flatpickr(".datepicker", {
+  "plugins": [new rangePlugin({ input: "#booking_start_date"})]
+  "disable": {
+    dateFormat: "Y-m-d",
+    disable: [
+        {
+            from: @start_date,
+            to: @end_date
+        }]
+  }
+})
