3ba2a8c80f7a4dd1e6c9c984fad154f47fe36542
Maria Antonia Rojas <marojaslondono@gmail.com>
2019-05-21 12:56:59 +0100

    comment

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,12 +4,12 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin.js"
 
 flatpickr(".datepicker", {
   "plugins": [new rangePlugin({ input: "#booking_start_date"})]
-  "disable": {
-    dateFormat: "Y-m-d",
-    disable: [
-        {
-            from: @start_date,
-            to: @end_date
-        }]
-  }
+  // "disable": {
+  //   dateFormat: "Y-m-d",
+  //   disable: [
+  //       {
+  //           from: @start_date,
+  //           to: @end_date
+  //       }]
+  // }
 })
