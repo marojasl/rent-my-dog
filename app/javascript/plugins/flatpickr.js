134c866d1834c66fd8bbdfa1b519022decc6acde
Maria Antonia Rojas <marojaslondono@gmail.com>
2019-05-21 14:36:17 +0100

    Disabled unavailable dates in booking calendar

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,14 +2,12 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin.js"
 
+const form = document.getElementById('new_booking');
+const dates = JSON.parse(form.dataset['dates']);
+
 flatpickr(".datepicker", {
-  "plugins": [new rangePlugin({ input: "#booking_start_date"})]
-  // "disable": {
-  //   dateFormat: "Y-m-d",
-  //   disable: [
-  //       {
-  //           from: @start_date,
-  //           to: @end_date
-  //       }]
-  // }
+  "plugins": [new rangePlugin({ input: "#booking_end_date"})],
+  dateFormat: "Y-m-d",
+  disable: dates,
+  minDate: 'today'
 })
